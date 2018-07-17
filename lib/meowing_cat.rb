bc7a19596c4d65f55afd2491e6a9aa5a976f0949
Sharon Mun <sharonymun@gmail.com>
2018-07-17 20:05:32 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,17 +1,17 @@
 class Cat
-  attr_accessor :actual_cats_name
+  attr_accessor :name
   
   def meow
     puts "meow!"
   end
-  def name=(cats_name)
-    @actual_cats_name = cats_name
-  end
-  def name
-    @actual_cats_name
-  end
+  # def name=(cats_name)
+  #   @actual_cats_name = cats_name
+  # end
+  # def name
+  #   @actual_cats_name
+  # end
 end
 
 maru = Cat.new
-maru.actual_cats_name = "Maru"
+maru.name = "Maru"
 maru.meow
